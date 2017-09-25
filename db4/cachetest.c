c549e6be90ae46938a183ef054e48533538a05f1
David Harris <dhharris9@gmail.com>
2017-09-25 10:16:36 +0200

    Optimized for temporal locality

--- db4/cachetest.c
+++ db4/cachetest.c
@@ -1,15 +1,16 @@
 /*  COPYING:
- *  Dave Patterson at UCB modified this code from a program by Andrea 
- *  Dusseau of U.C. Berkeley,which was based on a description in 
+ *  Dave Patterson at UCB modified this code from a program by Andrea
+ *  Dusseau of U.C. Berkeley,which was based on a description in
  *  Saavedra-Barrera [1992]:
- *    Saavedra-Barrera, R. H. [1992]. CPU Performance Evaluation and 
- *    Execution Time Prediction Using Narrow Spectrum Benchmarking, 
+ *    Saavedra-Barrera, R. H. [1992]. CPU Performance Evaluation and
+ *    Execution Time Prediction Using Narrow Spectrum Benchmarking,
  *    Ph.D. Dissertation, University of Calif., Berkeley (May).
  *  Patterson has given permission to use and modify this code as long
  *  as these people and the U. of Cal., Berkeley get proper credit.
  */
 
 #include <stdio.h>
+#include <assert.h>
 #include <sys/times.h>
 #include <sys/types.h>
 #include <time.h>
@@ -20,7 +21,7 @@
 #define CLK_TCK 60 /* number clock ticks per second */
 #endif
 int x[CACHE_MAX]; /* array going to stride through */
- 
+
 double get_seconds() { /* routine to read time */
     struct tms rusage;
     times(&rusage); /* UNIX utility: time in clock ticks */
@@ -31,35 +32,41 @@ int main() {
     int register i, index, stride, limit;
     int temp;
     int steps, tsteps, csize;
+    int accesses;
     double sec0, sec; /* timing variables */
- 
-    for (csize=CACHE_MIN; csize <= CACHE_MAX; csize=csize*2) 
+
+    for (csize=CACHE_MIN; csize <= CACHE_MAX; csize=csize*2)
         for (stride=1; stride <= 128; stride=stride*2) {
             sec = 0; /* initialize timer */
             limit = csize-stride+1; /* cache size this loop */
-     
+
             steps = 0;
             do { /* repeat until collect 1 second */
                 sec0 = get_seconds(); /* start timer */
-                for (i=SAMPLE*stride;i!=0;i=i-1) /* larger sample */
-                    for (index=0; index < limit; index=index+stride) 
+                accesses = 0;
+                for (index=0; index < limit; index=index+stride) {
+                    for (i=SAMPLE*stride;i!=0;i=i-1) /* larger sample */ {
                         x[index] = x[index] + 1; /* cache access */
+                        accesses += 1;
+                    }
+                }
+                assert(accesses == csize * SAMPLE);
                 steps = steps + 1; /* count while loop iterations */
                 sec = sec + (get_seconds() - sec0);/* end timer */
 
             } while (sec < 1.0); /* until collect 1 second */
-     
+
             /* Repeat empty loop to loop subtract overhead */
             tsteps = 0; /* used to match no. while iterations */
             do { /* repeat until same no. iterations as above */
                 sec0 = get_seconds(); /* start timer */
                 for (i=SAMPLE*stride;i!=0;i=i-1) /* larger sample */
-                    for (index=0; index < limit; index=index+stride) 
+                    for (index=0; index < limit; index=index+stride)
                         temp = temp + index; /* dummy code */
                 tsteps = tsteps + 1; /* count while iterations */
                 sec = sec - (get_seconds() - sec0);/* - overhead */
             } while (tsteps<steps); /* until = no. iterations */
-     
+
             printf("Size (bytes):%9d, Stride (bytes):%4d, read+write:%6.2f ns\n",
                csize*sizeof(int), stride*sizeof(int),
                (double) sec*1e9/(steps*SAMPLE*stride*((limit-1)/stride+1)));
