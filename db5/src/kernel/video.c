656901a500e3325744bec6f0ea6afe75abe8d6a9
Hugh Harris <dhharris9@gmail.com>
2017-10-03 10:37:22 +0200

    Drastically improve readability in video.c

--- db5/src/kernel/video.c
+++ db5/src/kernel/video.c
@@ -3,45 +3,43 @@
 
    This file is subject to the terms and conditions defined in
    file 'LICENSE', which is part of this source code package.
- */
+   */
 
 /*! \file video.c This file holds implementations of functions
   presenting output to the VGA screen. */
 #include <stdint.h>
 
-/*! Max number of columns in the VGA buffer. */
-#define MAX_COLS                (80)
-/*! Max number of columns in the VGA buffer. */
-#define MAX_ROWS                (25)
+/* Max dimensions of the VGA screen */
+#define MAX_COLS (80)
+#define MAX_ROWS (25)
 
-struct screen_position
-{
- unsigned char character; /*!< The character part of the byte tuple used for
-                               each screen position. */
- unsigned char attribute; /*!< The character part of the byte tuple used for
-                               each screen position. */
-};
-/*!< Defines a VGA text mode screen position. */
+typedef struct {
+        unsigned char character; /*!< The character part of the byte tuple
+                                   used for each screen position. */
+        unsigned char attribute; /*!< The attribute part of the byte tuple
+                                   used for each screen position. */
+} screen_position; /*!< Defines a VGA text mode screen position. */
 
-struct screen
-{
- struct screen_position positions[MAX_ROWS][MAX_COLS];
- /*!< The VGA screen. It is organized as a two dimensional array. */
-};
-/*!< Defines a VGA text mode screen. */
+typedef struct {
+        /*!< The VGA screen. It is organized as a two dimensional array. */
+        screen_position positions[MAX_ROWS][MAX_COLS];
+} screen; /*!< Defines a VGA text mode screen. */
 
 /*! points to the VGA screen. */
-static struct screen* const
-screen_pointer = (struct screen*) 0xB8000;
+static screen* const screen_pointer = (screen*) 0xB8000;
 
-void
-kprints(const char* string)
+/**
+ * Print a C string to the VGA screen
+ */
+void kprints(const char* string)
 {
- /* Implement me! */
+        /* Implement me! */
 }
 
-void
-kprinthex(const register uint32_t value)
+/**
+ * Writes a 32 bit unsigned value in hex to the VGA screen
+ */
+void kprinthex(const register uint32_t value)
 {
- /* Implement me! */
+        /* Implement me! */
 }
