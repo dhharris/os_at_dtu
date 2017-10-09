68c158ac0d8913e61d00784bbcd891a89c6b4595
Hugh Harris <dhharris9@gmail.com>
2017-10-09 09:08:11 +0200

    Formatted kernel_customization.c

--- db6/src/kernel/kernel_customization.c
+++ db6/src/kernel/kernel_customization.c
@@ -1,8 +1,8 @@
 /* Copyright (c) 1997-2017, FenixOS Developers
-   All Rights Reserved.
-
-   This file is subject to the terms and conditions defined in
-   file 'LICENSE', which is part of this source code package.
+ * All Rights Reserved.
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
  */
 
 /*! \file kernel_customization.c This file holds definitions
@@ -16,32 +16,31 @@
 
 void kernel_late_init(void)
 {
- /* Set up the first thread. For now we do not set up a process. That is
-   for you to do later. */
- threads[0].eip = executable_table[0];
- 
- /* Go to user space. */
- go_to_user_space();
+        /* Set up the first thread. For now we do not set up a process. That is
+           for you to do later. */
+        threads[0].eip = executable_table[0];
+
+        /* Go to user space. */
+        go_to_user_space();
 }
 
 void handle_system_call(void)
 {
- switch (current_thread->eax)
- {
-  case SYSCALL_VERSION:
-  {
-   current_thread->eax = 0x00010000;
-   break;
-  }
+        switch (current_thread->eax) {
+                case SYSCALL_VERSION:
+                        {
+                                current_thread->eax = 0x00010000;
+                                break;
+                        }
 
-  default:
-  {
-   /* Unrecognized system call. Not good. */
-   current_thread->eax = ERROR_ILLEGAL_SYSCALL;
-  }
- }
+                default:
+                        {
+                                /* Unrecognized system call. Not good. */
+                                current_thread->eax = ERROR_ILLEGAL_SYSCALL;
+                        }
+        }
 
- go_to_user_space();
+        go_to_user_space();
 }
 
 /* Add more definitions here if you need. */
