62d8ca683fa5dfdec8fef4b627f9c8aa525eb5cf
Hugh Harris <dhharris9@gmail.com>
2017-10-19 22:00:53 +0200

    Formatted kernel_customization.h

--- db6/src/kernel/kernel_customization.h
+++ db6/src/kernel/kernel_customization.h
@@ -1,8 +1,8 @@
 /* Copyright (c) 1997-2017, FenixOS Developers
-   All Rights Reserved.
-
-   This file is subject to the terms and conditions defined in
-   file 'LICENSE', which is part of this source code package.
+ * All Rights Reserved.
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
  */
 
 /*! \file kernel_customization.h This file holds declarations
@@ -11,24 +11,23 @@
 /* This file gets included into kernel.h! */
 
 /*! Defines a thread. */
-struct thread
-{
- uint32_t eax;
- uint32_t ebx;
- uint32_t esi;
- uint32_t edi;
- uint32_t ebp;
- uint32_t esp;
- uint32_t eip;
- /* The above members must be the first in the struct. Do not change the
-    order. */
+struct thread {
+        uint32_t eax;
+        uint32_t ebx;
+        uint32_t esi;
+        uint32_t edi;
+        uint32_t ebp;
+        uint32_t esp;
+        uint32_t eip;
+        /* The above members must be the first in the struct. Do not change the
+           order. */
 
- struct process* process;
- /*!< owning process of this thread. */
- struct thread*  next;
- /*!< can be used to implement linked lists of threads. */
+        struct process* process;
+        /*!< owning process of this thread. */
+        struct thread*  next;
+        /*!< can be used to implement linked lists of threads. */
 
- /* Add more members here if you need. */
+        /* Add more members here if you need. */
 };
 
 
@@ -38,10 +37,9 @@ struct thread
 #define PROCESS_BLOCKED 2
 
 /*! Defines a process. */
-struct process
-{
- int number_of_threads; /*!< the number of threads this process owns. */
- int state; /*!< Current state of the process */
+struct process {
+        int number_of_threads; /*!< the number of threads this process owns. */
+        int state; /*!< Current state of the process */
 };
 
 /* You can add more declarations here if you need. */
