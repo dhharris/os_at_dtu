3c6da37d4e7120749df388463b050690059bcb67
Hugh Harris <dhharris9@gmail.com>
2017-11-03 11:48:11 +0100

    Add unit tests for createprocess syscall

--- myOS/src/program_0/main.c
+++ myOS/src/program_0/main.c
@@ -1,9 +1,11 @@
-/*! \file \brief The first user program - simply tests the thread creation routine
- *
+/*
+ * This file includes all unit tests
  */
 #include <scwrapper.h>
+#include <minunit.h>
 
 int counter;
+int tests_run = 0;
 
 void thread(void)
 {
@@ -15,17 +17,40 @@ void thread(void)
 
 char thread_stack[4096];
 
+static char *test_createprocess_should_return_ALL_OK()
+{
+        int result = createprocess(1);
+        mu_assert("error, result != ALL_OK", result == ALL_OK);
+        return 0;
+}
+
+static char *test_createprocess_should_run_program_1()
+{
+        // If program 1 is ran then "Pong" will be printed to the screen
+        // if we perform a context switch
+        yield();
+        return 0;
+}
+
+static char *all_tests()
+{
+        mu_run_test(test_createprocess_should_return_ALL_OK);
+        mu_run_test(test_createprocess_should_run_program_1);
+        return 0;
+}
+
 int main(int argc, char* argv[])
 {
-        int i;
-        for (i = 0; i < 5; ++i)
-                if (ALL_OK != createthread(thread, thread_stack+4096)) {
-                        prints("createthread failed!\n");
-                        return 1;
-                }
+        char *result = all_tests();
+        if (result != 0) {
+                prints(result);
+                prints("\n");
+        } else {
+                prints("ALL TESTS PASSED\n");
+        }
+        prints("Tests run: ");
+        printhex(tests_run);
+        prints("\n");
 
-        while(1)
-                yield();
-
-        return 0;
+        while (1);
 }
